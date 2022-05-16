22fb4b6510b9d09c203bb8a97c0be35fd6e0def3
230443 <230443@edu.p.lodz.pl>
2022-05-16 14:34:57 +0200

    Add UserGetAgeTest.

--- src/test/java/be/pxl/auctions/model/UserGetAgeTest.java
+++ src/test/java/be/pxl/auctions/model/UserGetAgeTest.java
@@ -2,22 +2,39 @@ package be.pxl.auctions.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
+import static be.pxl.auctions.builder.UserBuilder.anUser;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserGetAgeTest {
 
 
 	@Test
 	public void returnsCorrectAgeWhenHavingBirthdayToday() {
-		// TODO implement test
+		User user = anUser()
+				.withDateOfBirth(LocalDate.now().minusYears(20))
+				.build();
+
+		assertEquals(20, user.getAge());
 	}
 
 	@Test
 	public void returnsCorrectAgeWhenHavingBirthdayTomorrow() {
-		// TODO implement test
+		User user = anUser()
+				.withDateOfBirth(LocalDate.now().minusYears(20).plusDays(1))
+				.build();
+
+		assertEquals(19, user.getAge());
 	}
 
 	@Test
 	public void returnsCorrectAgeWhenBirthdayWasYesterday() {
-		// TODO implement test
+		User user = anUser()
+				.withDateOfBirth(LocalDate.now().minusYears(20).minusDays(1))
+				.build();
+
+		assertEquals(20, user.getAge());
 	}
 
 }
